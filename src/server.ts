eae8ecd487051a8591d7e64d5535af570b8ddd00
Alessandro Ricci <106540146+AlexRicci93@users.noreply.github.com>
2022-11-22 12:52:36 +0100

    Split up the server to make it testable

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,7 @@
-import express from "express";
-import "express-async-errors";
-
-const app = express();
-
-app.get("/", (request, response) => {
-    response.send("This is the Space Facts API!");
-});
+import app from "./app";
 
 const port = 3000;
 
 app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}` )
+    console.log(`[server]: Server is running at http://localhost:${port}`);
 });
